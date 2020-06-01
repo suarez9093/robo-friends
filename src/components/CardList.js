5ce0f7dfdc67b87c9ff6d1fed7f29ce59d7c89ed
Alex <suarez9093@hotmail.com>
2020-06-01 09:23:23 -0600

    updated error conditional

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -6,7 +6,7 @@ function CardList() {
 
     const { filteredRobot } = useContext(SearchContext)
 
-    return !filteredRobot.length ? <h2 className="title">Loading...</h2> :
+    return filteredRobot.length === 0 ? <h2 className="title">Loading...</h2> :
         (
             filteredRobot.map(bot => {
                 return <Card key={bot.id} bot={bot} />
