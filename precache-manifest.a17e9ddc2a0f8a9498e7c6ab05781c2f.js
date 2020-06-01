f1348aa4c3b28ee9f387a0e21b2ad06582c5b835
Alex <suarez9093@hotmail.com>
2020-06-01 09:15:05 -0600

    Updates

---
+++ precache-manifest.a17e9ddc2a0f8a9498e7c6ab05781c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b6cd18811ff34c5dc954807168f15b",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "005c358e13af63dadff3",
+    "url": "/robo-friends/static/css/main.44323693.chunk.css"
+  },
+  {
+    "revision": "4f13f8d1dd49801b6c6d",
+    "url": "/robo-friends/static/js/2.ea409e94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.ea409e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005c358e13af63dadff3",
+    "url": "/robo-friends/static/js/main.53e464fe.chunk.js"
+  },
+  {
+    "revision": "3f75afefe0792e109c28",
+    "url": "/robo-friends/static/js/runtime-main.b414033a.js"
+  }
+]);
